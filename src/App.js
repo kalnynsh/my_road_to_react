2dc51dbae5300e64fe97cbcdb22111398b511da4
kalnynsh <kda869@yandex.ru>
2018-08-07 13:42:40 +0300

    Using const for column styling.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const largeColumn = {
+  width: '40%',
+};
+
+const midColumn = {
+  width: '30%',
+};
+
+const smallColumn = {
+  width: '10%',
+};
+
 const list = [
   {
     title: 'React',
@@ -69,19 +81,19 @@ const Table = ({ list, pattern, onDismiss }) => (
     {
       list.filter(isSearched(pattern)).map(item => (
         <div key={item.objectID} className="table-row">
-          <span style={{ width: '40%' }}>
+          <span style={largeColumn}>
             <a href="{item.url}">{item.title}</a>
           </span>
-          <span style={{ width: '30%' }}>
+          <span style={midColumn}>
             {item.author}
           </span>
-          <span style={{ width: '10%' }}>
+          <span style={smallColumn}>
             {item.num_comments}
           </span>
-          <span style={{ width: '10%' }}>
+          <span style={smallColumn}>
             {item.points}
           </span>
-          <span style={{ width: '10%' }}>
+          <span style={smallColumn}>
             <Button
               onClick={() => onDismiss(item.objectID)}
               className="button-inline btn-primary"
