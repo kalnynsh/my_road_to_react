5aeff736d65b28d248e9b17bf861fb8b016c61d8
kalnynsh <kda869@yandex.ru>
2018-08-09 17:50:40 +0300

    Update onDissmiss method using spread operator.

--- src/App.js
+++ src/App.js
@@ -105,8 +105,11 @@ class App extends Component
   onDismiss(id) {
     const isNotId = item => item.objectID !== id;
     const updatedHits = this.state.result.hits.filter(isNotId);
+    // this.setState({
+    //   result: Object.assign({}, this.state.result, { hits: updatedHits })
+    // });
     this.setState({
-      result: Object.assign({}, this.state.result, { hits: updatedHits })
+      result: { ...this.state.result, hits: updatedHits }
     });
   }
 
